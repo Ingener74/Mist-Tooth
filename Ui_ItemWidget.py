5b8dd79209d2d16a29e5abd0d34611b477e09b12
Shnaider Pavel <shnaiderpasha@gmail.com>
2020-01-31 17:30:52 +0500

    Поправил вид пункта загрузки

--- Ui_ItemWidget.py
+++ Ui_ItemWidget.py
@@ -19,14 +19,25 @@ class Ui_ItemWidget(object):
     def setupUi(self, ItemWidget):
         if ItemWidget.objectName():
             ItemWidget.setObjectName(u"ItemWidget")
-        ItemWidget.resize(505, 163)
-        self.horizontalLayout = QHBoxLayout(ItemWidget)
-        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        ItemWidget.resize(182, 121)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Maximum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(ItemWidget.sizePolicy().hasHeightForWidth())
+        ItemWidget.setSizePolicy(sizePolicy)
+        self.horizontalLayout_2 = QHBoxLayout(ItemWidget)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
         self.labelThumbnail = QLabel(ItemWidget)
         self.labelThumbnail.setObjectName(u"labelThumbnail")
+        self.labelThumbnail.setMaximumSize(QSize(16777215, 100))
+        self.labelThumbnail.setPixmap(QPixmap(u":/main/loading.png"))
 
-        self.horizontalLayout.addWidget(self.labelThumbnail)
+        self.horizontalLayout_2.addWidget(self.labelThumbnail)
 
+        self.verticalLayout_3 = QVBoxLayout()
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
+        self.horizontalLayout = QHBoxLayout()
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.labelTitle = QLabel(ItemWidget)
@@ -39,44 +50,38 @@ class Ui_ItemWidget(object):
 
         self.verticalLayout_2.addWidget(self.labelInfo)
 
-        self.progressBar = QProgressBar(ItemWidget)
-        self.progressBar.setObjectName(u"progressBar")
-        self.progressBar.setValue(24)
-
-        self.verticalLayout_2.addWidget(self.progressBar)
-
 
         self.horizontalLayout.addLayout(self.verticalLayout_2)
 
         self.verticalLayout = QVBoxLayout()
         self.verticalLayout.setObjectName(u"verticalLayout")
+        self.pushButtonStop = QPushButton(ItemWidget)
+        self.pushButtonStop.setObjectName(u"pushButtonStop")
+        icon = QIcon()
+        icon.addFile(u":/main/close.png", QSize(), QIcon.Normal, QIcon.Off)
+        self.pushButtonStop.setIcon(icon)
+        self.pushButtonStop.setIconSize(QSize(16, 16))
+
+        self.verticalLayout.addWidget(self.pushButtonStop)
+
         self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
 
         self.verticalLayout.addItem(self.verticalSpacer)
 
-        self.pushButtonStop = QPushButton(ItemWidget)
-        self.pushButtonStop.setObjectName(u"pushButtonStop")
-        sizePolicy = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.pushButtonStop.sizePolicy().hasHeightForWidth())
-        self.pushButtonStop.setSizePolicy(sizePolicy)
-        self.pushButtonStop.setMinimumSize(QSize(60, 60))
-        self.pushButtonStop.setMaximumSize(QSize(60, 60))
-        self.pushButtonStop.setBaseSize(QSize(0, 0))
-        icon = QIcon()
-        icon.addFile(u":/main/close.png", QSize(), QIcon.Normal, QIcon.Off)
-        self.pushButtonStop.setIcon(icon)
-        self.pushButtonStop.setIconSize(QSize(32, 32))
-
-        self.verticalLayout.addWidget(self.pushButtonStop)
-
-        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Minimum, QSizePolicy.Expanding)
-
-        self.verticalLayout.addItem(self.verticalSpacer_2)
-
 
         self.horizontalLayout.addLayout(self.verticalLayout)
+
+
+        self.verticalLayout_3.addLayout(self.horizontalLayout)
+
+        self.progressBar = QProgressBar(ItemWidget)
+        self.progressBar.setObjectName(u"progressBar")
+        self.progressBar.setValue(24)
+
+        self.verticalLayout_3.addWidget(self.progressBar)
+
+
+        self.horizontalLayout_2.addLayout(self.verticalLayout_3)
 
 
         self.retranslateUi(ItemWidget)
@@ -86,13 +91,16 @@ class Ui_ItemWidget(object):
 
     def retranslateUi(self, ItemWidget):
         ItemWidget.setWindowTitle(QCoreApplication.translate("ItemWidget", u"Form", None))
-        self.labelThumbnail.setText("")
-        self.labelTitle.setText("")
-        self.labelInfo.setText("")
+        self.labelThumbnail.setText(QCoreApplication.translate("ItemWidget", u"Thumbnail", None))
+        self.labelTitle.setText(QCoreApplication.translate("ItemWidget", u"Title", None))
+        self.labelInfo.setText(QCoreApplication.translate("ItemWidget", u"Info", None))
 #if QT_CONFIG(tooltip)
         self.pushButtonStop.setToolTip(QCoreApplication.translate("ItemWidget", u"\u041e\u0441\u0442\u0430\u043d\u043e\u0432\u0438\u0442\u044c", None))
 #endif // QT_CONFIG(tooltip)
         self.pushButtonStop.setText("")
+#if QT_CONFIG(shortcut)
+        self.pushButtonStop.setShortcut(QCoreApplication.translate("ItemWidget", u"Ctrl+S", None))
+#endif // QT_CONFIG(shortcut)
     # retranslateUi
 
 from resources_rc import *
